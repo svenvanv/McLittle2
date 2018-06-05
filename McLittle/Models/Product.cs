a2fb0a5ec2cc47726652bf4dad4d8e86f83f64d7
svenvanv <svenvanvelzen75@gmail.com>
2018-06-05 12:37:39 +0200

    diverseClasses

---
+++ McLittle/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace McLittle.Models
+{
+    public class Product
+    {
+        public int productId { get; set; }
+        public long EAN { get; set; }
+        public string title { get; set; }
+        public string brand { get; set; }
+        public string shortDesc { get; set; }
+        public string fullDesc { get; set; }
+        public string imageLink { get; set; }
+        public string weight { get; set; }
+        public float price { get; set; }
+        public string category { get; set; }
+        public string subCategory { get; set; }
+        public string subsubCategory { get; set; }
+    }
+}
---
+++ McLittle/Models/SubSubCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace McLittle.Models
+{
+    public class SubSubCategory
+    {
+        public int SubSubCategoryId { get; set; }
+        public string Title { get; set; }
+    }
+}
