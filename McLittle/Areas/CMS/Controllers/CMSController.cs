cc266d5e934e45f467343511e6cc5f024ef66eb3
svenvanv <svenvanvelzen75@gmail.com>
2018-06-11 10:51:56 +0200

    Templateshit

---
+++ McLittle/Areas/CMS/Controllers/CMSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace McLittle.Areas.CMS.Controllers
+{
+    public class CMSController : Controller
+    {
+        // GET: CMS/CMS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
