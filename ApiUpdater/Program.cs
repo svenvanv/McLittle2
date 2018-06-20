9310d3b8571365b146ef95193c028762e817ab86
svenvanv <svenvanvelzen75@gmail.com>
2018-06-20 14:19:06 +0200

    push

--- ApiUpdater/Program.cs
+++ ApiUpdater/Program.cs
@@ -76,50 +76,65 @@ namespace ApiUpdater
                 }
             }
         }
-            static void Product()
-            {
-                Entities e = new Entities();
-
-                XmlDocument doc = new XmlDocument();
-                doc.Load(baseUri + "products");
-
-                XmlNode node = doc.SelectSingleNode("products");
-
-                //foreach (XmlNode category in node.ChildNodes)
-                //{
-
-                //}
 
 
-                foreach (XmlNode products in node.ChildNodes)
-                {
-                    foreach (XmlNode product in products.ChildNodes)
-                    {
+             static void Product()
+        {
+            Entities e = new Entities();
 
-                        Product p = new Product();
-                        p.EAN = Convert.ToInt64(product.SelectSingleNode("EAN").InnerXml);
-                        p.title = product.SelectSingleNode("Title").InnerXml;
-                        p.brand = product.SelectSingleNode("Brand").InnerXml;
-                        p.shortDesc = product.SelectSingleNode("Shortdescription").InnerXml;
-                        p.fullDesc = product.SelectSingleNode("Fulldescription").InnerXml;
-                        p.imageLink = product.SelectSingleNode("Imageink").InnerXml;
-                        p.weight = product.SelectSingleNode("Weight").InnerXml;
-                        p.price = Convert.ToSingle(product.SelectSingleNode("Price").InnerXml);
-                        p.category = product.SelectSingleNode("Category").InnerXml;
-                        p.subCategory = product.SelectSingleNode("Subcategory").InnerXml;
-                        p.subsubCategory = product.SelectSingleNode("Subsubcategory").InnerXml;
+            XmlDocument doc = new XmlDocument();
+            doc.Load(baseUri + "products");
 
-                        long EAN = Convert.ToUInt32(product.SelectSingleNode("EAN").InnerXml);
-                        Product ssc = e.Products.FirstOrDefault(jap => jap.EAN == EAN);
-                        if (ssc == null)
-                        {
-                            e.Products.Add(ssc);
-                            e.SaveChanges();
-                        }
-                    }
-                }
-            }
+            XmlNode node = doc.SelectSingleNode("products");
+
         }
+
+
+
+        //static void Product()
+        //{
+        //    Entities e = new Entities();
+
+        //    XmlDocument doc = new XmlDocument();
+        //    doc.Load(baseUri + "products");
+
+        //    XmlNode node = doc.SelectSingleNode("products");
+
+        //    //foreach (XmlNode category in node.ChildNodes)
+        //    //{
+
+        //    //}
+
+
+        //    foreach (XmlNode products in node.ChildNodes)
+        //    {
+        //        foreach (XmlNode product in products.ChildNodes)
+        //        {
+
+        //        Product p = new Product();
+        //            p.EAN = Convert.ToInt64(product.SelectSingleNode("EAN").InnerXml);
+        //            p.title = product.SelectSingleNode("Title").InnerXml;
+        //            p.brand = product.SelectSingleNode("Brand").InnerXml;
+        //            p.shortDesc = product.SelectSingleNode("Shortdescription").InnerXml;
+        //            p.fullDesc = product.SelectSingleNode("Fulldescription").InnerXml;
+        //            p.imageLink = product.SelectSingleNode("Imageink").InnerXml;
+        //            p.weight = product.SelectSingleNode("Weight").InnerXml;
+        //            p.price = Convert.ToSingle(product.SelectSingleNode("Price").InnerXml);
+        //            p.category = product.SelectSingleNode("Category").InnerXml;
+        //            p.subCategory = product.SelectSingleNode("Subcategory").InnerXml;
+        //            p.subsubCategory = product.SelectSingleNode("Subsubcategory").InnerXml;
+
+        //            long EAN = Convert.ToUInt32(product.SelectSingleNode("EAN").InnerXml);
+        //            Product ssc = e.Products.FirstOrDefault(jap => jap.EAN == EAN);
+        //            if (ssc == null)
+        //            {
+        //                e.Products.Add(ssc);
+        //                e.SaveChanges();
+        //            }
+        //        }
+        //    }
+        //}
+    }
     }
 
 
